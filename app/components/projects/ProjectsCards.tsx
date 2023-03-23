90fc73df97d94590ab15d9775eba5ba1af7ef26a
shoaib memon <shoaibattari83@gmail.com>
2023-03-23 23:08:42 +0500

    button size responsive

--- app/components/projects/ProjectsCards.tsx
+++ app/components/projects/ProjectsCards.tsx
@@ -63,13 +63,13 @@ const projects: Proj[] = [
 
 function ProjectsCards() {
   return (
-    <div className="grid-cols-1  md:grid md:grid-cols-2 lg:grid-cols-3   ">
+    <div className=" grid-cols-1  md:grid md:grid-cols-1 lg:grid-cols-2   ">
       {projects.map((ele) => (
-        <div className="border-2 rounded-md pb-5 m-5 mt-10" key={ele.id}>
+        <div className="border-2 rounded-md pb-5 m-5 mt-10 " key={ele.id}>
           <div>
             <Image
               src={ele.Image}
-              width={500}
+              width={700}
               height={300}
               alt={ele.Alt}
               className="rounded-md  hover:scale-105 delay-100"
@@ -77,16 +77,16 @@ function ProjectsCards() {
           </div>
 
           <div className="p-5 ">
-            <h3 className="text-gray-400 font-serif ">{ele.text1}</h3>
-            <h2 className=" text-3xl font-bold text-gray-800 hover:text-orange-500 h-14 mt-2">
+            <h3 className=" text-2xl text-gray-400 font-serif ">{ele.text1}</h3>
+            <h2 className="text-2xl md:text-3xl font-bold text-gray-800 hover:text-orange-500 h-14 mt-2">
               {ele.text2}
             </h2>
           </div>
           <div className=" md:grid md:grid-cols-2 ">
-            <div className="mt-3  ">
+            <div className="mt-3 ">
               <Button text={"Visit Site"} link={ele.Site} />
             </div>
-            <div className="mt-3 ">
+            <div className="mt-3 w-60">
               <Button text={"View Github"} link={ele.Github} />
             </div>
           </div>
