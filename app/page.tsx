1faa6b641d5f6e50a056831e83479d66dafd7381
shoaib memon <shoaibattari83@gmail.com>
2023-05-11 01:20:32 +0500

    edit

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,13 @@ import Projects from "./components/projects/Projects";
 import Quolification from "./components/skills/Skills";
 import Services from "./components/services/Services";
 import Info from "./components/info/Info";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
 
 export default function Home() {
   return (
     <>
+    
       <Hero />
       <About />
       <Quolification />
